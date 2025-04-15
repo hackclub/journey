e645a3958efbf9a35d4d5be1c5eea7358351d195
Kartikey Chauhan <me@cskartikey.dev>
2025-04-15 12:41:11 +0530

    reduce the load time to 0.5 seconds

--- app/javascript/controllers/project_loader_controller.js
+++ app/javascript/controllers/project_loader_controller.js
@@ -4,7 +4,7 @@ export default class extends Controller {
   static targets = ["loadingIndicator", "content", "title"]
 
   async connect() {
-    await new Promise(resolve => setTimeout(resolve, 800))
+    await new Promise(resolve => setTimeout(resolve, 500))
     this.loadingIndicatorTarget.classList.add("hidden")
     this.contentTarget.classList.remove("hidden")
     this.titleTarget.classList.remove("hidden")
