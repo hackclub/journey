5fba92adfd3d36679991ac89f9c8457f37fc1b73
Kartikey Chauhan <me@cskartikey.dev>
2025-05-23 23:19:01 +0530

    add rich text to comments!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_23_005007) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_23_141941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_23_005007) do
     t.bigint "update_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.jsonb "rich_content"
     t.index ["update_id"], name: "index_comments_on_update_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
